e50b1f590a8e0dfeef066b68dde626104cf92c07
Mackley <tqi_mmagalhaes@uolinc.com>
2022-07-08 16:00:39 -0300

    Initial Commit

---
+++ Shared/TQIEasy_2App.swift
@@ -0,0 +1,17 @@
+//
+//  TQIEasy_2App.swift
+//  Shared
+//
+//  Created by Mackley Magalhães da Silva on 08/07/22.
+//
+
+import SwiftUI
+
+@main
+struct TQIEasy_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
